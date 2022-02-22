fdf70ba398aca82a0a2fa0a727dbbc8efd165bd9
Alena Samus <elensawelu@gmail.com>
2022-02-22 17:23:49 +0300

    Изменения

--- Sea_battle/Sea_battle/Form1.cs
+++ Sea_battle/Sea_battle/Form1.cs
@@ -22,17 +22,121 @@ namespace Sea_battle
         char[,] HomeMap = new char[11, 11];
         char[,] EnemyMap = new char[11, 11];
 
+        //Генерация карт
+        public void GenerateMap(char[,] Map)
+        {//Маркер того, что корабль ставить можно
+            bool Possible;
+
+            //В начале делаем клетки пустыми
+            for (int i = 0; i < 11; i++)
+                for (int j = 0; j < 11; j++)
+                    Map[i, j] = Chars[0];
+
+
+
+            //Считаем количество поставленных кораблей
+            int Ships = 0;
+
+            //Определяем текущую длину корабля. Начинаем с самого длинного
+            int Shiplength = 4;
+            do
+            {
+                //Выбираем ориентацию корабля: 0 - horizontal, 1 - vertical
+                int Orientation = rnd.Next(2);
+                //Выбираем координаты кораблей, предотвращая их выход за пределы игрового поля
+                //Выбираем верхнюю левую границу
+                int x = rnd.Next(1, 11 - Orientation * (Shiplength - 1));
+                int y = rnd.Next(1, 11 - (1 - Orientation) * (Shiplength - 1));
+
+                //Проверяем возможность поставить корабль
+                Possible = true; //Предполагаем, что корабль поставить можно
+                for (int i = 0; i < Shiplength; i++) //Идем по клеткам, где предоложительно можно поставить корабль
+                    if (Map[x + i * Orientation, y + i * (1 - Orientation)] != Chars[0])//Если там не спокойная вода
+                        Possible = false;//корабль поставить нельзя
+
+                //если есть безопасное место для корабля
+                if (Possible == true)
+
+                {//Заполняем "зоны близости" кораблей на карте
+                    for (int i = 0; i < Shiplength; i++)
+                    {//Матрица 3х3 из сбитых клеток
+                        for (int xx = Math.Max(x + i * Orientation - 1, 1);
+                            xx <= Math.Min(x + i * Orientation + 1, 10); xx++)
+                            for (int yy = Math.Max(y + i * (1 - Orientation) - 1, 1);
+                                yy <= Math.Min(y + i * (1 - Orientation) + 1, 10); yy++)
+                                Map[xx, yy] = Chars[1];
+                    }
+
+
+                    //Рисуем корабли
+                    for (int i = 0; i < Shiplength; i++)
+                        Map[x + i * Orientation, y + i * (1 - Orientation)] = Chars[2];
+
+                    //Увеличиваем количество кораблей
+
+                    Ships++;
+
+                    //Уменьшаем длину кораблей
+                    //Должно быть: 1 - четырехпалубный; 2 - трехпалубных; 3 - двухпалубных; 4 - однопалубных
+                    if (Ships == 1 || Ships == 3 || Ships == 6)
+                        Shiplength--;
+
+                }
+            } while (Ships < 10);
+
+            //После того как корабли расставили, убираем ограничители
+            for (int i = 0; i < 11; i++)
+                for (int j = 0; j < 11; j++)
+                {
+                    if (Map[i, j] == Chars[1])
+                        Map[i, j] = Chars[0];
+                }
+
+        }
+
         //Метод для отображения карты на форме
         public void ShowMap(char[,] Map, TableLayoutPanel TP)
         {
-            for (int i = 0; i < 11; i++)
-                for (int j = 0; j < 11; j++)
+            for (int i = 1; i < 11; i++)
+                for (int j = 1; j < 11; j++)
                 {
                     Label a = TP.Controls[i * 11 + j] as Label;
                     a.Text = Convert.ToString(Map[i, j]);
                 }
-                    
 
+        }
+
+        //Метод для выстрелов по карте противника
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //выясняем по какой клетке кликнул игрок
+            Label a = sender as Label;
+            int i;
+            for (i = 0; i < 121; i++)
+            {
+                Label b = EnemyTP.Controls[i] as Label;
+                if (a == b)
+                    break;
+
+                //если на карте, по которой стреляли, оказалась пустота, рисуем волну
+                if (EnemyMap[i % 11, i % 11] == Chars[0])
+                {
+                    EnemyMap[i % 11, i % 11] = Chars[1];
+                    a.Text = Convert.ToString(Chars[1]);
+                }
+
+                //передаем ход противнику
+                // EnemyMove();
+
+                //если на карте противника по координатам несбитый корабль, по рисуем сбитую клетку
+                if (EnemyMap[i % 11, i % 11] == Chars[2])
+                {
+                    EnemyMap[i % 11, i % 11] = Chars[3];
+                    a.Text = Convert.ToString(Chars[3]);
+                }
+
+
+            }
         }
 
         public Form1()
@@ -44,7 +148,7 @@ namespace Sea_battle
         {
             //Заполнение таблицы игрока метками
             for (int i = 0; i < 11; i++)
-                for(int j = 0; j < 11; j++)
+                for (int j = 0; j < 11; j++)
                 {
                     Label a = new Label();
                     a.Dock = DockStyle.Fill;//
@@ -65,6 +169,8 @@ namespace Sea_battle
                     a.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
                     a.Font = new System.Drawing.Font("wingdings", 18);
                     a.Text = Convert.ToString(Chars[0]);
+                    if (i > 0 && j > 0)
+                        a.Click += label1_Click;//назначаем в качесве обработчика метод для выстрелов по карте противника
                     EnemyTP.Controls.Add(a, j, i);
                 }
 
@@ -75,14 +181,13 @@ namespace Sea_battle
                 a.Font = new System.Drawing.Font("Arial", 12);
                 a.Text = Convert.ToString(Title[j - 1]);
 
-            }  
-            
+            }
+
             for (int i = 1; i < 11; i++)
             {
                 Label a = HomeTP.Controls[i * 11] as Label;//Номер строки умножить на ширину таблицы
-                a.Font = new System.Drawing.Font("Arial", 10);
+                a.Font = new System.Drawing.Font("Arial", 8);
                 a.Text = Convert.ToString(i);
-               
 
             }
 
@@ -98,18 +203,22 @@ namespace Sea_battle
             for (int i = 1; i < 11; i++)
             {
                 Label a = EnemyTP.Controls[i * 11] as Label;
-                a.Font = new System.Drawing.Font("Arial", 10);
+                a.Font = new System.Drawing.Font("Arial", 8);
                 a.Text = Convert.ToString(i);
 
             }
-            //Генерация карт
+            //Генерируем карты
+            GenerateMap(HomeMap);
+            GenerateMap(EnemyMap);
 
             //Отображаем карты
             ShowMap(HomeMap, HomeTP);
-            ShowMap(EnemyMap, EnemyTP);
+
+            ShowMap(EnemyMap, EnemyTP);// карту противника не показываем
 
         }
 
 
     }
 }
+
